ba39f44c02403ab9afe93899bfb672a3116e9f53
Luke Yamasaki <lukeyamasaki@gmail.com>
2022-04-13 13:52:15 -0600

    Added endpoints

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,24 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+
+
 import logo from './logo.svg';
 import styles from './Styles/App.module.css'
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const dispatch = useDispatch();
+
   return (
-    <div className={styles.App}>
-      <header className={styles.App-header}>
-        <img src={logo} className={styles.App-logo} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className={styles.App-link}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/'>
+          <h1>Home</h1>
+        </Route>
+        <Route exact path='/about'>
+          <h1>About</h1>
+        </Route>
+        <Route exact path='/questions'>
+          <h1>Questions</h1>
+        </Route>
+        <Route exact path='/nonprofits'>
+          <h1>All nonprofits</h1>
+        </Route>
+        <Route exact path='/businesses'>
+          <h1>All businesses</h1>
+        </Route>
+        <Route exact path='/categories'>
+          <h1>All categories</h1>
+        </Route>
+        <Route exact path='/users'>
+          <h1>All users</h1>
+        </Route>
+        <Route exact path='/settings'>
+          <h1>Settings</h1>
+        </Route>
+        <Route exact path='/inbox'>
+          <h1>Messages</h1>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 

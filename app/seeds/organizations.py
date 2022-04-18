2bcf8ed358abe73bd22dc978a418f256ab130a3c
Luke Yamasaki <lukeyamasaki@gmail.com>
2022-04-18 12:28:54 -0600

    Changed the organizations model

--- app/seeds/organizations.py
+++ app/seeds/organizations.py
@@ -25,6 +25,60 @@ def seed_organizations():
     )
     db.session.add(mealize)
 
+    food_bank_of_the_rockies = Organization(
+        federalId='84-0772672',
+        isNonprofit=True,
+        logoUrl='https://media.9news.com/assets/KUSA/images/b75b0ffc-d633-46b1-83e7-36fb5bc9e4a3/b75b0ffc-d633-46b1-83e7-36fb5bc9e4a3_1140x641.png',
+        imageUrl='https://media.9news.com/assets/KUSA/images/dc308017-8594-40d0-93c8-e2808c9b9ed5/dc308017-8594-40d0-93c8-e2808c9b9ed5_1140x641.jpg',
+        open='8:00',
+        close='16:00',
+        name='Food Bank of the Rockies',
+        description="We are the largest hunger-relief organization in the Rocky Mountain region. That makes us uniquely suited to answer the enormous challenge of hunger, which affects 1 out of 8 people in our communities. Since 1978, we've put the power of community to work for our neighbors in need. Through partnerships. Through programs. Through people. Through you.",
+        street='10700 E 45th Ave',
+        zip='80239',
+        city='Denver',
+        state='Colorado',
+        phone='3033719250',
+        email='donatenow@foodbankrockies.org'
+    )
+    db.session.add(food_bank_of_the_rockies)
+
+    bienvdenidos_food_bank = Organization(
+        federalId='84-0772672',
+        isNonprofit=True,
+        logoUrl='http://www.bienvenidosfoodbank.org/wp-content/uploads/2017/12/LogoBienvenidos.png',
+        imageUrl='https://media.9news.com/assets/KUSA/images/dc308017-8594-40d0-93c8-e2808c9b9ed5/dc308017-8594-40d0-93c8-e2808c9b9ed5_1140x641.jpg',
+        open='11:00',
+        close='17:30',
+        name='Food Bank of the Rockies',
+        description="For more than 40 years, the Bienvenidos Food Bank has offered emergency food assistance to people in need. If a family is in crisis and needs food we are available no matter where they come from.",
+        street='3810 N Pecos St',
+        zip='80211',
+        city='Denver',
+        state='Colorado',
+        phone='3034336328',
+        email='director@bienvenidosfoodbank.org'
+    )
+    db.session.add(bienvdenidos_food_bank)
+
+    denver_inner_city_parish = Organization(
+        federalId='84-0525768',
+        isNonprofit=True,
+        logoUrl='https://static.wixstatic.com/media/0198fd_29758a36fd2a4afda85e6abfb73e7a56~mv2.png/v1/fill/w_159,h_145,al_c,usm_0.66_1.00_0.01,enc_auto/DICP_logo_White%20Bird_White%20Logo_edited_p.png',
+        imageUrl='https://static.wixstatic.com/media/0198fd_3303f729b95c46bd80066fac7d0e3940~mv2.jpg/v1/fill/w_2543,h_787,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/0198fd_3303f729b95c46bd80066fac7d0e3940~mv2.jpg',
+        open='9:00',
+        close='13:00',
+        name='The Denver Inner City Parish',
+        description="The Denver Inner City Parish loves and supports individuals and families in our community, empowering them to break the cycle of poverty. The word parish means “community” and the DICP is committed to community. We are a human services nonprofit. The Parish elevates community through its community programs.",
+        street='1212 Mariposa Street',
+        zip='80204',
+        city='Denver',
+        state='Colorado',
+        phone='3036290636',
+        email='communications@dicp.org'
+    )
+    db.session.add(denver_inner_city_parish)
+
     mealize_market = Organization(
         federalId='88-8888888',
         isNonprofit=False,
@@ -44,82 +98,6 @@ def seed_organizations():
     )
     db.session.add(mealize_market)
 
-    food_bank_of_the_rockies = Organization(
-        federalId='84-0772672',
-        isNonprofit=True,
-        logoUrl='https://media.9news.com/assets/KUSA/images/b75b0ffc-d633-46b1-83e7-36fb5bc9e4a3/b75b0ffc-d633-46b1-83e7-36fb5bc9e4a3_1140x641.png',
-        imageUrl='https://media.9news.com/assets/KUSA/images/dc308017-8594-40d0-93c8-e2808c9b9ed5/dc308017-8594-40d0-93c8-e2808c9b9ed5_1140x641.jpg',
-        open='8:00',
-        close='16:00',
-        name='Food Bank of the Rockies',
-        description="We are the largest hunger-relief organization in the Rocky Mountain region. That makes us uniquely suited to answer the enormous challenge of hunger, which affects 1 out of 8 people in our communities. Since 1978, we've put the power of community to work for our neighbors in need. Through partnerships. Through programs. Through people. Through you.",
-        street='10700 E 45th Ave',
-        unit='',
-        zip='80239',
-        city='Denver',
-        state='Colorado',
-        phone='3033719250',
-        email='donatenow@foodbankrockies.org'
-    )
-    db.session.add(food_bank_of_the_rockies)
-
-    bienvdenidos_food_bank = Organization(
-        federalId='84-0772672',
-        isNonprofit=True,
-        logoUrl='http://www.bienvenidosfoodbank.org/wp-content/uploads/2017/12/LogoBienvenidos.png',
-        imageUrl='https://media.9news.com/assets/KUSA/images/dc308017-8594-40d0-93c8-e2808c9b9ed5/dc308017-8594-40d0-93c8-e2808c9b9ed5_1140x641.jpg',
-        open='11:00',
-        close='17:30',
-        name='Food Bank of the Rockies',
-        description="For more than 40 years, the Bienvenidos Food Bank has offered emergency food assistance to people in need. If a family is in crisis and needs food we are available no matter where they come from.",
-        street='3810 N Pecos St',
-        unit='',
-        zip='80211',
-        city='Denver',
-        state='Colorado',
-        phone='3034336328',
-        email='director@bienvenidosfoodbank.org'
-    )
-    db.session.add(bienvdenidos_food_bank)
-
-    denver_inner_city_parish = Organization(
-        federalId='84-0525768',
-        isNonprofit=True,
-        logoUrl='https://static.wixstatic.com/media/0198fd_29758a36fd2a4afda85e6abfb73e7a56~mv2.png/v1/fill/w_159,h_145,al_c,usm_0.66_1.00_0.01,enc_auto/DICP_logo_White%20Bird_White%20Logo_edited_p.png',
-        imageUrl='https://static.wixstatic.com/media/0198fd_3303f729b95c46bd80066fac7d0e3940~mv2.jpg/v1/fill/w_2543,h_787,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/0198fd_3303f729b95c46bd80066fac7d0e3940~mv2.jpg',
-        open='9:00',
-        close='13:00',
-        name='The Denver Inner City Parish',
-        description="The Denver Inner City Parish loves and supports individuals and families in our community, empowering them to break the cycle of poverty. The word parish means “community” and the DICP is committed to community. We are a human services nonprofit. The Parish elevates community through its community programs.",
-        street='1212 Mariposa Street',
-        unit='',
-        zip='80204',
-        city='Denver',
-        state='Colorado',
-        phone='3036290636',
-        email='communications@dicp.org'
-    )
-    db.session.add(denver_inner_city_parish)
-
-    for i in range(6, 27):
-        nonprofits = Organization(
-            federalId='84-0525768',
-            isNonprofit=True,
-            logoUrl='https://static.wixstatic.com/media/0198fd_29758a36fd2a4afda85e6abfb73e7a56~mv2.png/v1/fill/w_159,h_145,al_c,usm_0.66_1.00_0.01,enc_auto/DICP_logo_White%20Bird_White%20Logo_edited_p.png',
-            imageUrl='https://static.wixstatic.com/media/0198fd_3303f729b95c46bd80066fac7d0e3940~mv2.jpg/v1/fill/w_2543,h_787,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/0198fd_3303f729b95c46bd80066fac7d0e3940~mv2.jpg',
-            open='9:00',
-            close='13:00',
-            name='The Denver Inner City Parish',
-            description="The Denver Inner City Parish loves and supports individuals and families in our community, empowering them to break the cycle of poverty. The word parish means “community” and the DICP is committed to community. We are a human services nonprofit. The Parish elevates community through its community programs.",
-            street='1212 Mariposa Street',
-            unit='',
-            zip='80204',
-            city='Denver',
-            state='Colorado',
-            phone='3036290636',
-            email='communications@dicp.org'
-        )
-
     walmart = Organization(
         federalId='71-0415188',
         isNonprofit=False,
@@ -130,7 +108,6 @@ def seed_organizations():
         name='Walmart Inc.',
         description="Retail-variety Stores",
         street='702 Southwest 8th St',
-        unit='',
         zip='72716',
         city='Bentonville',
         state='Arkansas',
@@ -149,14 +126,13 @@ def seed_organizations():
         name='Walmart Inc.',
         description="Retail-variety Stores",
         street='702 Southwest 8th St',
-        unit='',
         zip='72716',
         city='Bentonville',
         state='Arkansas',
         phone='5012734000',
         email='service@walmartcontacts.com'
     )
-    db.session.add(walmart)
+    db.session.add(target)
 
     amazon = Organization(
         federalId='71-0415188',
@@ -168,7 +144,6 @@ def seed_organizations():
         name='Walmart Inc.',
         description="Retail-variety Stores",
         street='702 Southwest 8th St',
-        unit='',
         zip='72716',
         city='Bentonville',
         state='Arkansas',
@@ -177,69 +152,24 @@ def seed_organizations():
     )
     db.session.add(amazon)
 
-
-    for i in range(3, 27):
-        nonprofit_managers = Organization(
-            organizationId=i,
-            isNonprofit=True,
-            isManager=True,
-            private=False,
-            firstName=fake.first_name(),
-            lastName=fake.last_name(),
-            email=fake.unique.email(),
-            phone=phonenumbers.parse(fake.unique.phone_number(), 'US').national_number,
-            age=randint(18, 91),
-            deaf=False,
-            autism=False,
-            learningDisabled=False,
-            lgbtq=random() > 0.5,
-            profileImageUrl="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
-            jobDescription=fake.text(max_nb_chars=255),
-            hashedPassword=generate_password_hash(fake.password())
-        )
-        db.session.add(nonprofit_managers)
-
-    for j in range(27, 100):
-        business_managers = Organization(
-            organizationId=j,
+    for i in range(6, 27):
+        businesses = Organization(
+            federalId='84-0525768',
             isNonprofit=False,
-            isManager=True,
-            private=False,
-            firstName=fake.first_name(),
-            lastName=fake.last_name(),
-            email=fake.unique.email(),
-            phone=phonenumbers.parse(fake.unique.phone_number(), 'US').national_number,
-            age=randint(18, 91),
-            deaf=False,
-            autism=False,
-            learningDisabled=False,
-            lgbtq=random() > 0.5,
-            profileImageUrl="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
-            jobDescription=fake.text(max_nb_chars=255),
-            hashedPassword=generate_password_hash(fake.password())
+            logoUrl='https://static.wixstatic.com/media/0198fd_29758a36fd2a4afda85e6abfb73e7a56~mv2.png/v1/fill/w_159,h_145,al_c,usm_0.66_1.00_0.01,enc_auto/DICP_logo_White%20Bird_White%20Logo_edited_p.png',
+            imageUrl='https://static.wixstatic.com/media/0198fd_3303f729b95c46bd80066fac7d0e3940~mv2.jpg/v1/fill/w_2543,h_787,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/0198fd_3303f729b95c46bd80066fac7d0e3940~mv2.jpg',
+            open='10:00',
+            close='22:00',
+            name=fake.company(),
+            description=fake.catch_phrase(),
+            street=fake.street_address(),
+            zip='80204',
+            city='Denver',
+            state='Colorado',
+            phone='3036290636',
+            email='communications@dicp.org'
         )
-        db.session.add(business_managers)
-
-    for k in range(100, 200):
-        volunteers = Organization(
-            organizationId=randint(3, 27),
-            isNonprofit=True,
-            isManager=False,
-            private=random() > 0.5,
-            firstName=fake.first_name(),
-            lastName=fake.last_name(),
-            email=fake.unique.email(),
-            phone=phonenumbers.parse(fake.unique.phone_number(), 'US').national_number,
-            age=randint(18, 91),
-            deaf=random() > 0.5,
-            autism=random() > 0.5,
-            learningDisabled=random() > 0.5,
-            lgbtq=random() > 0.5,
-            profileImageUrl="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
-            jobDescription=fake.text(max_nb_chars=255),
-            hashedPassword=generate_password_hash(fake.password())
-        )
-        db.session.add(volunteers)
+        db.session.add(businesses)
 
     db.session.commit()
 

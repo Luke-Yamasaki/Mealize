9e38c6e3bf60ddc92c19673ff979c2f5b100bef4
Luke Yamasaki <lukeyamasaki@gmail.com>
2022-04-30 17:57:52 -0600

    Added localstorage helper function

--- react_app/src/Helpers/LocalStorage/index.js
+++ react_app/src/Helpers/LocalStorage/index.js
@@ -1,1 +1,29 @@
+import { useState } from 'react';
 
+export const useLocalStorage = (key, startingValue) => {
+    const [storedData, setStoredData] = useState(() => {
+        if(typeof window === 'undefined') {
+            return startingValue
+        }
+        try {
+            const data = window.localStorage.getItem(key);
+            return data ? JSON.parse(data) : startingValue;
+        } catch (error) {
+            console.log(error)
+            return startingValue;
+        }
+    });
+    const setSelection = (selection) => {
+        try {
+            const selectionToStore = selection instanceof Function ? selection(storedData) : selection;
+            setStoredData(selectionToStore);
+
+            if(typeof window !== 'undefined') {
+                window.localStorage.setItem(key, JSON.stringify(selectionToStore));
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    };
+    return [storedData, setSelection];
+};

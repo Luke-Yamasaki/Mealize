d78f175b007ea5b5911ee609866b66ed74509d33
Luke Yamasaki <lukeyamasaki@gmail.com>
2022-05-01 12:06:40 -0600

    Refactored the customization components

--- react_app/src/Components/Styled/Customization.js
+++ react_app/src/Components/Styled/Customization.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const LightCustomizationContainer = styled.div`
+export const CustomizationContainer = styled.div`
     width: 300px;
     height: 600px;
     display: flex;
@@ -17,24 +17,7 @@ export const LightCustomizationContainer = styled.div`
     font-weight: 700;
 `;
 
-export const DarkCustomizationContainer = styled.div`
-    width: 300px;
-    height: 600px;
-    display: flex;
-    flex-direction: column;
-    align-items: space-around;
-    justify-content: center;
-    border: 1px solid #616161;
-    border-radius: 5px;
-    background-color: #191919;
-    overflow: hidden;
-    color: white;
-    font-family: motiva-sans, sans-serif;
-    font-style: normal;
-    font-weight: 700;
-`;
-
-export const LightContentContainer = styled.div`
+export const ContentContainer = styled.div`
     width: 300px;
     height: 100px;
     display: flex;
@@ -45,29 +28,11 @@ export const LightContentContainer = styled.div`
     border-bottom: 1px solid #D5D5D5;
 `;
 
-export const DarkContentContainer = styled.div`
-    width: 300px;
-    height: 100px;
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-start;
-    align-items: center;
-    gap: 5px;
-    border-bottom: 1px solid #616161;
-`;
-
-export const LightLabel = styled.p`
+export const Label = styled.p`
     margin: 0px;
     padding: 0px;
     font-size: 1em;
     color: black;
-`;
-
-export const DarkLabel = styled.p`
-    margin: 0px;
-    padding: 0px;
-    font-size: 1em;
-    color: white;
 `;
 
 export const IconBox = styled.div`
--- react_app/src/Components/Styled/Dark/Customization.js
+++ react_app/src/Components/Styled/Dark/Customization.js
@@ -1,80 +1,14 @@
-
+import { CustomizationContainer, ContentContainer, Label } from '../Customization';
 import styled from 'styled-components';
 
-export const LightCustomizationContainer = styled.div`
-    width: 300px;
-    height: 600px;
-    display: flex;
-    flex-direction: column;
-    align-items: space-around;
-    justify-content: center;
-    border: 1px solid #D5D5D5;
-    border-radius: 5px;
-    background-color: #FFFFFF;
-    overflow: hidden;
-    color: black;
-    font-family: motiva-sans, sans-serif;
-    font-style: normal;
-    font-weight: 700;
-`;
-
-export const DarkCustomizationContainer = styled.div`
-    width: 300px;
-    height: 600px;
-    display: flex;
-    flex-direction: column;
-    align-items: space-around;
-    justify-content: center;
+export const DarkCustomizationContainer = styled(CustomizationContainer)`
     border: 1px solid #616161;
-    border-radius: 5px;
     background-color: #191919;
-    overflow: hidden;
     color: white;
-    font-family: motiva-sans, sans-serif;
-    font-style: normal;
-    font-weight: 700;
 `;
-
-export const LightContentContainer = styled.div`
-    width: 300px;
-    height: 100px;
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-start;
-    align-items: center;
-    gap: 5px;
-    border-bottom: 1px solid #D5D5D5;
-`;
-
-export const DarkContentContainer = styled.div`
-    width: 300px;
-    height: 100px;
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-start;
-    align-items: center;
-    gap: 5px;
+export const DarkContentContainer = styled(ContentContainer)`
     border-bottom: 1px solid #616161;
 `;
-
-export const LightLabel = styled.p`
-    margin: 0px;
-    padding: 0px;
-    font-size: 1em;
-    color: black;
-`;
-
-export const DarkLabel = styled.p`
-    margin: 0px;
-    padding: 0px;
-    font-size: 1em;
+export const DarkLabel = styled(Label)`
     color: white;
-`;
-
-export const IconBox = styled.div`
-    width: 50px;
-    height: 50px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
 `;

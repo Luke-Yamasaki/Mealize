07f36389040b1d6dc913df498b683acab29a19b7
Luke Yamasaki <lukeyamasaki@gmail.com>
2022-05-02 18:42:03 -0600

    Fixed colors

--- react_app/src/Components/Cards/Post/CardContent/category.js
+++ react_app/src/Components/Cards/Post/CardContent/category.js
@@ -23,23 +23,23 @@ export const category = {
     },
     'dark': {
         1: {
-            'background': 'linear-gradient(#15BAFA, #FFFFFF)',
+            'background': 'linear-gradient(#15BAFA, #366B80)',
             'border': '1px solid #15BAFA'
         },
         2: {
-            'background': 'linear-gradient(#6ED56C, #FFFFFF)',
+            'background': 'linear-gradient(#6ED56C, #286526)',
             'border': '1px solid #6ED56C'
         },
         3: {
-            'background': 'linear-gradient(#FB8DDB, #FFFFFF)',
+            'background': 'linear-gradient(#FB8DDB, #8F3676)',
             'border': '1px solid #FB8DDB'
         },
         4: {
-            'background': 'linear-gradient(#F1BD60, #FFFFFF)',
+            'background': 'linear-gradient(#F1BD60, #5F4E32)',
             'border': '1px solid #F1BD60'
         },
         5: {
-            'background': 'linear-gradient(#FF623E, #FFFFFF)',
+            'background': 'linear-gradient(#FF623E, #90311D)',
             'border': '1px solid #FF623E'
         }
     }

9d610f57891ce13f721e34f95b7894d4b595b6a5
Luke Yamasaki <lukeyamasaki@gmail.com>
2022-05-11 13:33:00 -0600

    Fixed dates

--- react-app/src/utils/Dates/index.js
+++ react-app/src/utils/Dates/index.js
@@ -24,50 +24,29 @@ export const daysAgo = (post) => {
 }
 
 // [1] Frbruary will be 29 on leap years
-const monthsAndDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+// const monthsAndDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-const determineLeapYears = (year) => {
-    const leapYears = [];
-    const max = year - 90;
-    const min = year - 18;
-    for(let i = min; i < max; i += 4) {
-        leapYears.push(i);
-    }
-    console.log(leapYears)
-    return leapYears;
-}
+// const determineLeapYears = (year) => {
+//     const leapYears = [];
+//     const max = year - 90;
+//     const min = year - 18;
+//     for(let i = min; i < max; i += 4) {
+//         leapYears.push(i);
+//     }
+//     console.log(leapYears)
+//     return leapYears;
+// }
 
 export const ageBoundary = () => {
     const year = new Date().getFullYear()
     const month = new Date().getMonth()
     const date = new Date().getDate()
 
-    let minMonth;
-    let yesterday = date - 1;
+    const formattedMonth = month <= 9 ? '0' + (month+1).toString() : (month+1).toString()
+    const formattedDate = date <= 9 ? `0${date}` : date.toString();
 
-    if(yesterday === 0) {
-        minMonth = month - 1;
-        if(minMonth === -1) {
-            minMonth = 11;
-        }
-        if(minMonth === 1) {
-            const leapYears = determineLeapYears(year);
-            yesterday = leapYears.includes(year) ? 29 : 28;
-        } else {
-            yesterday = monthsAndDays[minMonth]
-        }
-    } else {
-        minMonth = month;
-    }
-
-    const formattedMaxMonth = month <= 9 ? '0' + (month+1).toString() : (month+1).toString()
-    const formattedMaxDate = date <= 9 ? `0${date}` : date.toString();
-
-    const formattedMinMonth =minMonth <= 9 ? '0' + (minMonth+1).toString() : (minMonth+1).toString()
-    const formattedMinDate = yesterday <= 0 ? `0${yesterday}` : yesterday.toString();
-
-    const tooOld = (year - 90).toString() + '-' + formattedMaxMonth + '-' + formattedMaxDate;
-    const tooYoung = (year - 18).toString() + '-' + formattedMinMonth + '-' + formattedMinDate;
+    const tooOld = (year - 90).toString() + '-' + formattedMonth + '-' + formattedDate;
+    const tooYoung = (year - 18).toString() + '-' + formattedMonth + '-' + formattedDate;
 
     return {'old': tooOld, 'young': tooYoung};
 }

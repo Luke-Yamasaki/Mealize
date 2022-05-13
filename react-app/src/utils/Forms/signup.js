da1095cd2280a6152cbb02acaa498427404e9c8b
Luke Yamasaki <lukeyamasaki@gmail.com>
2022-05-13 09:20:39 -0600

    Debugging s3 config

--- react-app/src/utils/Forms/signup.js
+++ react-app/src/utils/Forms/signup.js
@@ -4,6 +4,7 @@ export const emailValidate = (email) => {
 };
 
 export const uploadProfileImage = async (formData) => {
+    console.log(formData)
     const response = await fetch('/api/auth/images', {
         method: "POST",
         body: formData,

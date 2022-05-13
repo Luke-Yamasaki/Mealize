0432593effb6a5d3ae347836932a1f09f4863dec
Luke Yamasaki <lukeyamasaki@gmail.com>
2022-05-13 00:36:52 -0600

    Need to add default placeholders for forms

--- react-app/src/Components/Preview/index.js
+++ react-app/src/Components/Preview/index.js
@@ -1,10 +1,13 @@
 import { PreviewBox } from "../Styled/PreviewSection"
 import { UserIdCard } from "../Cards/UserIdCard";
+import { ItemCard } from "../Cards/ItemCard";
 
 export const PreviewSection = ({ type, props }) => {
+    console.log(props)
     return (
         <PreviewBox>
             {type === 'id' && <UserIdCard props={props}/>}
+            {type === 'item' && <ItemCard props={props}/>}
         </PreviewBox>
     )
 };

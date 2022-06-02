703a30eb4a63a5c2155d191b0ffc6fa264a6cbc4
Luke Yamasaki <lukeyamasaki@gmail.com>
2022-06-02 12:41:14 -0600

    Welcome animation is complete and localstorage works now

--- react-app/src/Components/Styled/Welcome.js
+++ react-app/src/Components/Styled/Welcome.js
@@ -6,84 +6,133 @@ const lockin = keyframes`
         width: 100vw;
         height: 100vh;
     }
-    50% {
+    20% {
         width: 100vw;
         height: 100vh;
     }
-    75% {
-        width: 1600px;
-        height: 60px;
+    30% {
+        width: 100vw;
+        height: 100vh;
     }
-    90% {
+    40% {
+        width: 100vw;
+        height: 100vh;
+    }
+    70% {
         width: 1600px;
         height: 60px;
+        opacity: 100%;
+    }
+    85%{
+        width: 1600px;
+        height: 60px;
+        opacity: 0%;
     }
     100% {
+        opacity: 0%;
         width: 1600px;
         height: 0px;
-        display: none;
     }
 `;
 
-const lockinBG = () => css`${lockin} 4s ease-in-out 1 normal forwards`;
+const lockinBG = () => css`${lockin} 5s ease-in-out 1 normal forwards`;
 
 const shrink = keyframes`
     0% {
         width: 8357.219px;
         height: 7133.676px;
     }
-    25%{
+    20%{
         width: 592.167px;
         height: 505.471px;
     }
-    90% {
+    30% {
         width: 592.167px;
         height: 505.471px;
+    }
+    40% {
+        width: 592.167px;
+        height: 505.471px;
+        margin-left: 0px;
+    }
+    70% {
+        width: 45px;
+        height: 45px;
+        display: none;
+        margin-left: -1455px;
+        opacity: 100%;
+    }
+    85%{
+        width: 45px;
+        height: 45px;
+        display: none;
+        margin-left: -1455px;
+        opacity: 0%;
     }
     100% {
+        opacity: 0%;
         width: 0px;
         height: 0px;
         display: none;
+        margin: 0px 0px 0px -1455px;
     }
 `;
 
-const shrinkLogo = () => css`${shrink} 4s ease-in-out 1 normal forwards`;
+const shrinkLogo = () => css`${shrink} 5s ease-in-out 1 normal forwards`;
 
 const shrinkTitle = keyframes`
     0% {
         opacity: 0%;
         width: 500px;
         height: 300px;
-        font-size: 300px;
+        font-size: 150px;
+        margin: 900px 0px 0px -25px;
     }
-    25% {
+    20% {
         opacity: 0%;
         width: 500px;
         height: 300px;
-        font-size: 200px;
+        font-size: 150px;
+        margin: 900px 0px 0px -25px;
     }
-    50%{
+    30%{
         opacity: 100%;
         width: 500px;
         height: 300px;
-        font-size: 100px;
+        font-size: 150px;
+        margin: 900px 0px 0px -25px;
     }
-    90% {
+    40%{
         opacity: 100%;
         width: 500px;
         height: 300px;
-        font-size: 100px;
+        font-size: 150px;
+        margin: 900px 0px 0px -25px;
+    }
+    70% {
+        width: 100px;
+        height: 30px;
+        font-size: 28px;
+        margin: 10px 0px 0px -1278px;
+        opacity: 100%;
+    }
+    85%{
+        width: 100px;
+        height: 30px;
+        font-size: 28px;
+        margin: 10px 0px 0px -1278px;
+        opacity: 0%;
     }
     100% {
         opacity: 0%;
         width: 0px;
         height: 0px;
         font-size: 0px;
-        display: none;
+        margin: 10px 0px 0px -1278px;
     }
 `;
 
-const shrinkLogotype = () => css`${shrinkTitle} 4s ease-in-out 1 normal forwards`;
+const shrinkLogotype = () => css`${shrinkTitle} 5s ease-in-out 1 normal forwards`;
 
 export const WelcomeAnimation = styled.div`
     background: linear-gradient(#76D97E, #28A690);
@@ -102,11 +151,13 @@ export const LogoVectorBox = styled.div`
     justify-content: center;
     align-items: center;
     animation: ${shrinkLogo};
+    position: absolute;
 `;
 
 export const LogoType = styled(Black)`
     animation: ${shrinkLogotype};
     color: ${props => props.theme === 'light' ? 'white' : '#191919'};
+    position: absolute;
 `;
 
 export const WelcomeContent = styled.div`
@@ -117,4 +168,40 @@ export const WelcomeContent = styled.div`
     align-items: center;
     justify-content: center;
     background-color: ${props => props.theme === 'light' ? 'white' : '#191919'};
+`;
+
+const changeOpacity = keyframes`
+    from{
+        opacity: 0%;
+    }
+    to {
+        opacity: 100%;
+    }
+`;
+
+const reveal = css`${changeOpacity} 1.5s ease-in-out 1 normal forwards`;
+
+
+export const HandVectorBox = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    animation: ${reveal};
+    animation-delay: 4s;
+    width: ${props => props.square};
+    height: ${props => props.square};
+    opacity: 0%;
+`;
+
+export const GreetingText = styled(Black)`
+    width: 900px;
+    height: 100px;
+    font-size: 75px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: ${props => props.theme === 'light' ? '#191919' : 'white'};
+    animation: ${reveal};
+    animation-delay: 4s;
+    opacity: 0%;
 `;
--- react-app/src/Pages/Welcome/index.js
+++ react-app/src/Pages/Welcome/index.js
@@ -1,19 +1,25 @@
 //Hooks
+import { useSelector } from 'react-redux';
 import { useTheme } from '../../Context/ThemeContext';
 
 //Components
 import { Navbar } from '../../Components/Navbar';
 
 //Styled-components
-import { LogoType, LogoVectorBox, WelcomeAnimation, WelcomeContent } from '../../Components/Styled/Welcome';
-import { VectorBox } from '../../Components/Styled/Layout';
+import { LogoType, LogoVectorBox, WelcomeAnimation, WelcomeContent, HandVectorBox, GreetingText } from '../../Components/Styled/Welcome';
 
 //Icons
 import { Hand } from '../../Assets/Icons/Hand';
 import { XLLogo } from '../../Assets/Logo';
+import { Redirect } from 'react-router-dom';
 
 export const Welcome = () => {
+    const sessionUser = useSelector(state => state.session.user);
     const { theme } = useTheme();
+
+    if(sessionUser) {
+        return <Redirect to='/' />
+    }
 
     return (
         <>
@@ -25,9 +31,10 @@ export const Welcome = () => {
             </WelcomeAnimation>
             <Navbar theme={theme} />
             <WelcomeContent theme={theme} >
-                <VectorBox square='550px'>
+                <GreetingText theme={theme}>Welcome to Mealize!</GreetingText>
+                <HandVectorBox square='550px'>
                     <Hand theme={theme} />
-                </VectorBox>
+                </HandVectorBox>
             </WelcomeContent>
         </>
     )

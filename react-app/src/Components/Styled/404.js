07d29519a53f3af3e555493ec2be4b6fe60c38b6
Luke Yamasaki <lukeyamasaki@gmail.com>
2022-06-11 21:58:54 -0600

    Fixed the 404 page

--- react-app/src/Components/Styled/404.js
+++ react-app/src/Components/Styled/404.js
@@ -4,11 +4,11 @@ export const FourOFourContainer = styled.div`
     font-family: motiva-sans, sans-serif;
     font-weight: 900;
     padding-top: 20px;
-    font-size: 3em;
+    font-size: 2.5em;
     color: ${props => props.theme === 'light' ?'#191919' : 'white'};
     max-width: 1336px;
     width: 100vw;
-    height: 900px;
+    min-height: 65vh;
     background-color: ${props => props.theme === 'light' ? 'white' : '#191919'};
     display: flex;
     flex-direction: column;
@@ -24,4 +24,5 @@ export const FourOFourImage = styled.img`
     width: 70vw;
     max-height: 700px;
     height: 45vh;
+    margin-left: 60px;
 `;
